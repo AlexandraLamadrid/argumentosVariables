6c314c4f9e76d98fe19bdc2db42c84bd52eeec6a
Alexandra <155563331+AlexandraLamadrid@users.noreply.github.com>
2024-01-22 00:14:51 -0500

    Subiendo programa

---
+++ src/test/TestArgumentosVariables.java
@@ -0,0 +1,20 @@
+package test;
+//Argumentos variables es cuando vamos a pasar un argumento hacia un metodo.
+public class TestArgumentosVariables {
+    public static void main(String[] args) {
+        imprimirNumeros(3, 4, 5);
+        imprimirNumeros(1, 2);
+        variosParametros("Juan", 8,9,10);
+    }
+    
+    private static void variosParametros(String nombre, int... numeros){
+        System.out.println("nombre = " + nombre);
+        imprimirNumeros(numeros);
+    }
+    
+    private static void imprimirNumeros(int... numeros){
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("elemeto:" + numeros[i]);
+        }
+    }
+}
